834fda2bb778a67ae2c2f2b24edffe6ab64dc7d7
Daniel Kiss <flowfast47@gmail.com>
2023-09-22 12:32:44 -0700

    add a generated component

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent {
+  constructor() {}
+
+  ngOnInit() {}
+}
